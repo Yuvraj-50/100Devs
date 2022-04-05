4196f8954ed3a001dbeb3f5c6817cda424ecab86
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-04-05 11:18:18 +0530

    add solution

---
+++ codewars/8kyu/implemen-array-prototype-filter.js
@@ -0,0 +1,15 @@
+// solution /////////////////////
+// What we want to implement is Array.prototype.filter() function, just like the existing Array.prototype.filter(). Another similar function is _.filter() in underscore.js and lodash.js.
+
+// The usage will be quite simple, like:
+
+// [1, 2, 3, 4, 5].filter(num => num > 3) == [4, 5]
+// Of course, the existing Array.prototype.filter() function has been undefined for the purposes of this Kata.
+
+// solution /////////////////////////////////////////////////////
+
+Array.prototype.filter = function (func) {
+  let arr = [];
+  this.forEach((elem) => (func(elem) ? arr.push(elem) : 0));
+  return arr;
+};
