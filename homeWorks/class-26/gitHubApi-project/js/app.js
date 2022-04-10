2d43928add6b0fae494fdf6e9775c30812f004b3
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-04-11 01:12:46 +0530

    complete desktop view and javascript for gitHub api project

--- homeWorks/class-26/gitHubApi-project/js/app.js
+++ homeWorks/class-26/gitHubApi-project/js/app.js
@@ -20,94 +20,97 @@ togglers.forEach((toggler) => {
 ////////////////////////////////////// gettting and displaying user/////////////////////
 const card = document.querySelector(".card");
 
+// getting data from user input bar/////////
+const input_el = document.querySelector(".username-input");
+const SearchBtn = document.querySelector(".search");
+
+// get data from api //////////////////
+
 function getUserData(user) {
   fetch(`https://api.github.com/users/${user}`)
     .then((res) => res.json())
     .then((data) => {
+      // render data
       renderCard(data);
-      console.log(data);
     });
 }
 
+// variables for render data
+const Profileimg = document.querySelector(".user-img");
+
+const Name = document.querySelector(".name");
+const userName = document.querySelector(".user-name");
+const joined = document.querySelector(".join-date");
+
+const bio = document.querySelector(".bio");
+
+const noOfRepo = document.querySelector(".no-of-repo");
+const noOfFollowers = document.querySelector(".no-of-followers");
+const noOfFollowings = document.querySelector(".no-of-followings");
+
+const Location = document.querySelector(".location");
+const userUrl = document.querySelector(".user-url");
+const userTwitter = document.querySelector(".user-twitter");
+const userCompany = document.querySelector(".user-company");
+
 function renderCard(data) {
-  const html = `
-        <div class="profile-img-area">
-        <img src="https://img.icons8.com/color/150/000000/circled-user-male-skin-type-4--v1.png" />
-        </div>
-        <div class="stats-area">
-        <div class="name-details">
-            <div class="user-name-details">
-                <h2 class="name">Yuvraj-50</h2>
-                <p class="user-name">@Yuvraj-50</p>
-                <p class="bio">This profile has no bio</p>
-            </div>
-            <div class="joined-date">
-                <p class="date">joined 20 dec 2021</p>
-            </div>
-        </div>
-        <div class="user-followers-detail">
-            <div class="repo">
-                <p>Repo</p>
-                <h4>22</h4>
-            </div>
-            <div class="followers">
-                <p>followers</p>
-                <h4>2000</h4>
-            </div>
-            <div class="following">
-                <p>following</p>
-                <h4>100</h4>
-            </div>
-        </div>
-
-        <div class="social">
-            <div class="location-con social-item">
-                <svg class="location-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                    viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round">
-                    <circle cx="12" cy="10" r="3" />
-                    <path d="M12 21.7C17.3 17 20 13 20 10a8 8 0 1 0-16 0c0 3 2.7 6.9 8 11.7z" />
-                </svg>
-
-                <p class="location">chennai , india</p>
-            </div>
-            <div class="website social-item">
-                <svg class="website-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                    viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round">
-                    <path d="M10 13a5 5 0 0 0 7.54.54l3-3a5 5 0 0 0-7.07-7.07l-1.72 1.71"></path>
-                    <path d="M14 11a5 5 0 0 0-7.54-.54l-3 3a5 5 0 0 0 7.07 7.07l1.71-1.71"></path>
-                </svg>
-
-                <a href="yuvraj-me.netlify.com">yuvraj-me.netlify.com</a>
-            </div>
-            <div class="twitter social-item">
-                <svg class="twitter-icon" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                    viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round">
-                    <path
-                        d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z">
-                    </path>
-                </svg>
-
-                <a href="https://yuvraj505">yuvraj505</a>
-            </div>
-            <div class="company social-item">
-                <svg class="company" xmlns="http://www.w3.org/2000/svg" width="24" height="24"
-                    viewBox="0 0 24 24" fill="none" stroke="#000000" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round">
-                    <path d="M20 9v11a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V9" />
-                    <path d="M9 22V12h6v10M2 10.6L12 2l10 8.6" />
-                </svg>
-
-                <p class="company">No company</p>
-            </div>
-        </div>
-
-        </div>
-    `;
-
-  card.insertAdjacentHTML("beforeend", html);
+  Profileimg.src = data.avatar_url;
+  Name.textContent = data.name;
+  userName.textContent = `@${data.login}`;
+  userName.href = data.html_url;
+  joined.textContent = `joined ${getDate(data.created_at)}`;
+  bio.textContent = `${
+    data.bio == null ? "This profile has no bio" : data.bio
+  }`;
+  noOfRepo.textContent = `${data.public_repos}`.padStart(2, "0");
+  noOfFollowers.textContent = `${data.followers}`.padStart(2, "0");
+  noOfFollowings.textContent = `${data.following}`.padStart(2, "0");
+  Location.textContent = getSocials(data, "location");
+  userUrl.textContent = getSocials(data, "blog");
+  userTwitter.textContent = getSocials(data, "twitter_username");
+  getTwitterWebsiteUrl(data, "blog", userUrl);
+  getTwitterWebsiteUrl(data, "twitter_username", userTwitter);
+  userCompany.textContent = getSocials(data, "company");
 }
 
-getUserData("yuvraj");
+function getDate(timeZone) {
+  const newDate = new Date(timeZone);
+  const options = {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+  };
+  return newDate.toLocaleString("en", options);
+}
+
+function getTwitterWebsiteUrl(data, task, link) {
+  if (data[task] == "" || data[task] == null) {
+    link.removeAttribute("href");
+  } else if (task == "blog") {
+    if (data[task].includes("http")) {
+      link.href = data[task];
+    } else {
+      link.href = `https://${data[task]}`;
+    }
+  } else {
+    link.href = `https://twitter.com/${data[task]}`;
+  }
+}
+
+function swichToUserGitHub(data, task, link) {
+  link.href = data[task];
+}
+
+function getSocials(data, task) {
+  return data[task] == "" || data[task] == null
+    ? `Not available`
+    : `${data[task]}`;
+}
+
+getUserData("yuvraj-50");
+
+SearchBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!input_el.value) return;
+  getUserData(input_el.value);
+});
