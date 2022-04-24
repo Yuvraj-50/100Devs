cc5a32f057f8c1e597909a5ea12f54e7fa76e611
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-04-24 13:07:04 +0530

    24 - 4  - 2022

---
+++ codewars/8kyu/name-shuffler.js
@@ -0,0 +1,12 @@
+// question url  : - https://www.codewars.com/kata/559ac78160f0be07c200005a/train/javascript;
+
+// question instruction ////////////////////////////////////////
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+// nameShuffler('john McClane'); => "McClane john"
+
+// solution////////////////////////////////////
+function nameShuffler(str) {
+  return str.split(" ").reverse().join(" ");
+}
