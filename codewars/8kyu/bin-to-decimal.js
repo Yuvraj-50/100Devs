569d1e3b55c593a6fb1f0a17ae19a361a8b6798e
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-04-25 20:40:37 +0530

    25 - 4 - 2022

---
+++ codewars/8kyu/bin-to-decimal.js
@@ -0,0 +1,11 @@
+// question url :: -- https://www.codewars.com/kata/57a5c31ce298a7e6b7000334/train/javascript
+
+// quesiton instruction :::: ----------------------------
+
+// Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// solution :::::::::::::: -------------------------------
+
+function binToDec(bin) {
+  return parseInt(bin, 2);
+}
