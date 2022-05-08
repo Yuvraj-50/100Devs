fa0dbcbc27e373cd1511d0b67b979ddd25a70587
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-05-09 00:20:20 +0530

    9 - 5- 2022

---
+++ codewars/8kyu/basic-variable-assignment.js
@@ -0,0 +1,11 @@
+// quesiton url :::-- https://www.codewars.com/kata/50ee6b0bdeab583673000025/train/javascript
+
+// question instruction  ::::::::::::::
+
+// This code should store "codewa.rs" as a variable called name but it's not working. Can you figure out why?
+
+// solution ::::::::::::::::::::::::::::::::::::::::
+
+var a = "code";
+var b = "wa.rs";
+var name = a + b;
