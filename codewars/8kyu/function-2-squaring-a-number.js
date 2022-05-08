1f612f5b4f3a88ab23518ef4a1fbfcdcac51dab1
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-05-09 00:40:02 +0530

    9 - 5- 2022

---
+++ codewars/8kyu/function-2-squaring-a-number.js
@@ -0,0 +1,11 @@
+// question url ;; https://www.codewars.com/kata/523b623152af8a30c6000027/train/javascript
+
+// quesiton instruciton :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// Now you have to write a function that takes an argument and returns the square of it.
+
+// solution ///////////////////////////////////////////////////////////
+
+function square(num) {
+  return num ** 2;
+}
