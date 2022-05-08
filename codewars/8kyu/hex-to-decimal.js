24e989c8c0bb6a0f3426d19a5df3a791b2c7fbd5
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-05-08 12:09:20 +0530

    8 - 5 - 2022

---
+++ codewars/8kyu/hex-to-decimal.js
@@ -0,0 +1,11 @@
+// question url ::-- https://www.codewars.com/kata/57a4d500e298a7952100035d/train/javascript
+
+// question instruction '''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+// Complete the function which converts hex number (given as a string) to a decimal number.
+
+// solution ''''''''''''''''''''''''''''-'-----------''-'-'-'-'-'-'-'-'-'-'-''-
+
+function hexToDec(hexString) {
+  return parseInt(hexString, 16);
+}
