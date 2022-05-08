36635a52ea0bad71ed9193104f7a326cc62c9fd6
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-05-08 12:40:07 +0530

    8 - 5 - 2022

---
+++ codewars/8kyu/hello-name-world.js
@@ -0,0 +1,24 @@
+// question url :::--- https://www.codewars.com/kata/57e3f79c9cb119374600046b/train/javascript
+
+// question instruction////////////////////////////////////////////////////////////////
+
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+
+// Examples:
+
+// * With `name` = "john"  => return "Hello, John!"
+// * With `name` = "aliCE" => return "Hello, Alice!"
+// * With `name` not given
+//   or `name` = ""        => return "Hello, World!"
+
+// solution //////////////////////////////////////////////////////////////////////
+
+function hello(name) {
+  return name
+    ? `Hello, ${
+        name.slice(0, 1).toUpperCase() + name.slice(1).toLowerCase() + "!"
+      }`
+    : "Hello, World!";
+}
