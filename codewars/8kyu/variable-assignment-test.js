52142ae85af9cfcd75f7a21eb74a5e07184833cd
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-05-11 20:12:20 +0530

    add solution

---
+++ codewars/8kyu/variable-assignment-test.js
@@ -0,0 +1,11 @@
+// question url :: https://www.codewars.com/kata/5612e743cab69fec6d000077/train/javascript
+// question instruction ::::::::::::;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+// solution :::::::::::::::::::::::::::::::::::
+
+var a = "dev";
+var b = "Lab";
+
+var name = a + b;
