0951d598409688cbad36aeeaf6d5b48878714f67
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-05-12 21:24:29 +0530

    12 - 5- 2022

---
+++ codewars/8kyu/remove-duplicate-from-list.js
@@ -0,0 +1,13 @@
+// quesiton url :: https://www.codewars.com/kata/57a5b0dfcf1fa526bb000118/train/javascript
+
+// question instruciton ::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// solution ::::::::::::::::::::::::::::::::::::::::::::
+
+function distinct(a) {
+  return [...new Set(a)];
+}
