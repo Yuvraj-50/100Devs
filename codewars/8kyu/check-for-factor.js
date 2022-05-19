63eaecdcdab6122716d7a080e36a277895120098
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-05-19 22:12:56 +0530

    19 - 5 - 2022

---
+++ codewars/8kyu/check-for-factor.js
@@ -0,0 +1,11 @@
+// question url :: -- https://www.codewars.com/kata/55cbc3586671f6aa070000fb/train/javascript
+
+// question instruction ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// This function should test if the factor is a factor of base.
+
+// solution :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function checkForFactor(base, factor) {
+  return base % factor == 0 ? true : false;
+}
