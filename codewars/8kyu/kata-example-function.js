833cde5c028c4f280aad9dac60c52e328946900a
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-05-21 22:38:30 +0530

    21 - 5 - 2022

---
+++ codewars/8kyu/kata-example-function.js
@@ -0,0 +1,16 @@
+// question url :: https://www.codewars.com/kata/525c1a07bb6dda6944000031/train/javascript
+
+// question instruction ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the array websites/Websites 1,000 times.
+
+// var websites = []
+
+// solution :::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+var websites = [];
+while (websites.length < 1000) {
+  websites.push("codewars");
+}
