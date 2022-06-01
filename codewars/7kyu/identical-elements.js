3111a5db1c6851af812ff19cda21a047df9b8268
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-06-01 13:25:46 +0530

    1 - 6 - 2022

---
+++ codewars/7kyu/identical-elements.js
@@ -0,0 +1,13 @@
+// question url : https://www.codewars.com/kata/583ebb9328a0c034490001ba/train/javascript
+
+// question instruction :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+// Given two arrays of integers m and n, test if they contain at least one identical element. Return true if they do; false if not.
+
+// Your code must handle any value within the range of a 32-bit integer, and must be capable of handling either array being empty (which is a false result, as there are no duplicated elements).
+
+// solution ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+function duplicateElements(m, n) {
+  return m.some((elem) => n.includes(elem));
+}
