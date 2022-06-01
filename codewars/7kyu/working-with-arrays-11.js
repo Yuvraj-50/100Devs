16dc1bff5435bda44d8fe73cb5fb4bd6e66ee4ac
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-06-01 13:28:57 +0530

    1 - 6 - 2022

---
+++ codewars/7kyu/working-with-arrays-11.js
@@ -0,0 +1,14 @@
+// question url : https://www.codewars.com/kata/5a7b3d08fd5777bf6a000121/train/javascript
+
+// question instruction :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
+// In this kata the function returns an array/list like the one passed to it but with its nth element removed (with 0 <= n <= array/list.length - 1). The function is already written for you and the basic tests pass, but random tests fail. Your task is to figure out why and fix it.
+
+// Good luck!
+
+// solution::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::;
+
+function removeNthElement(arr, n) {
+  var arrCopy = [...arr];
+  arrCopy.splice(n, 1);
+  return arrCopy;
+}
