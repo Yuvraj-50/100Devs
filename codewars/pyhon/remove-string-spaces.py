134f718356a495e43d557ed2c38f3d18b71727d3
yuvraj singh <yuvrajsingh2348q@gmail.com>
2022-06-12 10:52:22 +0530

    remvoe string spaces question and solution

---
+++ codewars/pyhon/remove-string-spaces.py
@@ -0,0 +1,12 @@
+# // question url :: https://www.codewars.com/kata/57eae20f5500ad98e50002c5/train/python
+
+# // question instruction ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# // Simple, remove the spaces from the string, then return the resultant string.
+
+# // solution :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+# solution one :::::
+def no_space(x):
+    return x.replace(' ' , '');
+    
